9678e5c7b932aba2334199e1da48db22e9550b91
Bruno Silveira <117918338+BrunoSilveira75@users.noreply.github.com>
2023-03-21 11:32:25 -0300

    Add files via upload

---
+++ Odd or Even Exercise.py
@@ -0,0 +1,7 @@
+number = int(input("Type a number: "))
+result = number % 2
+
+if result == 0:
+    print(f"The number {number} is an EVEN number.")
+else:
+    print(f"The number {number} is an ODD number.")
